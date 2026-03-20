1b8f1cf5be8f081c718cf466907268831f96161c
Avinash K. Pandey <avinash2pandey@gmail.com>
2026-03-20 18:00:19 +0530

    Project Setup

---
+++ Protecta EGPL/Protecta EGPLTests/Protecta_EGPLTests.swift
@@ -0,0 +1,17 @@
+//
+//  Protecta_EGPLTests.swift
+//  Protecta EGPLTests
+//
+//  Created by avinash pandey on 20/03/26.
+//
+
+import Testing
+@testable import Protecta_EGPL
+
+struct Protecta_EGPLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Protecta EGPL/Protecta EGPLUITests/Protecta_EGPLUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Protecta_EGPLUITestsLaunchTests.swift
+//  Protecta EGPLUITests
+//
+//  Created by avinash pandey on 20/03/26.
+//
+
+import XCTest
+
+final class Protecta_EGPLUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
